c7acf25d5c3dfa6597fcdd0627aa2712b96ff260
Ivonne Minchala <ivonneroxana16@gmail.com>
2024-12-03 16:46:20 -0500

    Fin seccion 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import{RouterModule} from  '@angular/router'
+import { FormsModule } from '@angular/forms';
+
+import { BaseChartDirective  } from 'ng2-charts';
+//Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
+import { Chart } from 'chart.js';
 
 
 
@@ -16,7 +23,7 @@ import { SharedModule } from '../shared/shared.module';
     DashboardComponent, 
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
   ],
   exports:[
     DashboardComponent, 
@@ -27,7 +34,11 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    BaseChartDirective,
+    RouterModule,
+    FormsModule,
+    ComponentsModule,
+    
   ]
 })
 export class PagesModule { }
